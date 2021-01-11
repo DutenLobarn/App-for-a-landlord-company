eb0776fbea114160f7c2e67e8e91959be158f4b7
Mandus Lindström <mandus85@hotmail.com>
2021-01-11 16:27:13 +0100

    Wrong folder got prepped

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
-
-const express = require('express')
-const app = express();
-const http = require('http').createServer(app);
+const express = require('express')
+const app = express();
+const http = require('http').createServer(app);
 
 app.use(express.static('public'))
-// Front-end
-// app.get('/', function (req, res) {​​​​​
-//   res.sendFile(__dirname + '/index.html')
-// }​​​​​)
-console.log(__dirname)
+// Front-end
+app.get('/', function (req, res) {res.sendFile(__dirname + '/index.html')})
 
-http.listen(process.env.PORT || 3000, () => {​​​​​
-  console.log('server is running')
-}​​​​​);
+// links
+app.get('/mina-sidor', function (req, res) {res.sendFile(__dirname + '/pages/my-pages.html')})
 
+http.listen(process.env.PORT || 3000, () => {console.log('server is running')});
